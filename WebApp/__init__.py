0a7b90fce7ba7b381126d7bacc1dffb3f5e2ca44
cicc-cicc <kinga040302@example.com>
2026-02-28 09:00:20 +0100

    ezek lesznek az alap adatbazisok, amiket majd a flask appban hasznalunk

---
+++ WebApp/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Globális objektumok
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    from .config import db_config
+    params = db_config()
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{params['user']}:{params['password']}@{params['host']}:{params['port']}/{params['database']}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'hotelguru_titok'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        from . import models
+        from . import routes
+
+    return app
